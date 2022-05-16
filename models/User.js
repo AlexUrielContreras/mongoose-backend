577f4f953d6ea52473253317253963cd7f95002c
Alex Contreras <alex.code29@gmail.com>
2022-05-15 19:09:06 -0500

    built the user schema

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    
+    username: {
+        type: String,
+        required: true,
+        // unique is not a validator just a convenient helper
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: '[a-z0-9]+@[a-z]+\.edu\.[a-z]{2,3}'
+    },
+    thoughts: [Thought],
+    friends: [this]
+},
+{
+    toJSON: {
+        virtual: true
+    },
+    id: true
+});
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+
+
+const User = model('User', UserSchema);
+
+module.exports = User
